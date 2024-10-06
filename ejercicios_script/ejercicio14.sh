c58faeede5a5837772a95edc0650dfca50eb5433
luctroort <luctroort@alu.edu.gva.es>
2024-10-06 18:40:35 +0200

    Update ejercicio14.sh

--- ejercicios_script/ejercicio14.sh
+++ ejercicios_script/ejercicio14.sh
@@ -4,6 +4,16 @@ $1=accion
 $2=nombre
 $3=apellido1
 $4=apellido2
-${5:-}=grupo
+$5=grupo
 
-case
+usuario="alu${apellido1:0:2}${apellido2:0:2}${nombre:0:1}"
+
+if [ $accion = "alta" ]; then
+  if [ -z $grupo ]; then
+    grupo=$user_id
+    grupoadd $grupo
+  fi
+  useradd -m -g $grupo -s /bin/bash $user_id
+elif [ $accion = "baja" ]; then
+  userdel -r $user_id
+fi
